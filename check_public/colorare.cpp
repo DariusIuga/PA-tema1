cede0826d6326f3f59983b2a0b8f4532f7bed8c3
DariusIuga <dariusiuga2@gmail.com>
2024-04-04 22:03:01 +0300

    15p la problema 2 colorare

---
+++ check_public/colorare.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned int get_result(vector<char> zones) {
+    // I'll use a greedy algorithm that calculates the number of possible combinations for k zones based on the number of combinations for k-1 zones, the type of the previous tile and the type of the current tile.
+
+    /* Base cases:
+
+    1) zones[0] == 'V' => nr_current = 3
+    2) zones[0] == 'H' => nr_current = 6
+
+    */
+    const unsigned int MOD = 1'000'000'007;
+    unsigned int nr_paintings;
+    if (zones[0] == 'V') {
+        nr_paintings = 3;
+    }
+    else {
+        nr_paintings = 6;
+    }
+
+    /* There are 4 general cases:
+
+    1) prev_zone == 'V', current_zone == 'V' => nr_current = 2 * nr_prev
+    2) prev_zone == 'V', current_zone == 'H' => nr_current = 2 * nr_prev
+    1) prev_zone == 'H', current_zone == 'V' => nr_current = 1 * nr_prev
+    1) prev_zone == 'H', current_zone == 'H' => nr_current = 3 * nr_prev
+
+    */
+    for (int i = 1;i < zones.size();i++) {
+        if (zones[i - 1] == 'V') {
+            nr_paintings = (nr_paintings * 2) % MOD;
+        }
+        else if (zones[i] == 'H') {
+            nr_paintings = (nr_paintings * 3) % MOD;
+        }
+    }
+
+    return nr_paintings;
+}
+
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    int nr_groups;
+
+    // Read input
+    ifstream in("colorare.in");
+    in >> nr_groups;
+
+    vector<char> zones;
+
+    for (int i = 0; i < nr_groups; i++) {
+        int nr_zones_in_group;
+        char type;
+        in >> nr_zones_in_group >> type;
+        for (int j = 0; j < nr_zones_in_group; j++) {
+            zones.push_back(type);
+        }
+    }
+    in.close();
+
+    // Write output
+    ofstream out("colorare.out");
+    out << get_result(zones) << "\n";
+
+
+    out.close();
+
+    return 0;
+}
--- check_public/servere.cpp
+++ check_public/servere.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int max_vector(vector<int> vec){
+int max_vector(vector<int> vec) {
     int max = vec[0];
 
-    for(int el : vec){
-        if(el > max){
+    for (int el : vec) {
+        if (el > max) {
             max = el;
         }
     }
@@ -13,11 +13,11 @@ int max_vector(vector<int> vec){
     return max;
 }
 
-double find_system_power(double current_choice, vector<int> power, vector<int> capacity){
+double find_system_power(double current_choice, vector<int> power, vector<int> capacity) {
     double min = power[0] - fabs(capacity[0] - current_choice);
-    for(int i = 1; i < power.size(); i++){
+    for (int i = 1; i < power.size(); i++) {
         double server_power = power[i] - fabs(capacity[i] - current_choice);
-        if(server_power < min){
+        if (server_power < min) {
             min = server_power;
         }
     }
@@ -25,48 +25,43 @@ double find_system_power(double current_choice, vector<int> power, vector<int> c
     return min;
 }
 
-double find_best(double lo, double hi, double old_result, vector<int> power, vector<int> capacity){
+double find_best(double lo, double hi, double old_result, vector<int> power, vector<int> capacity) {
     double mid = (lo + hi) / 2;
-    double lo_power = find_system_power(lo,power,capacity);
-    double mid_power = find_system_power(mid,power,capacity);
-    double hi_power = find_system_power(hi,power,capacity);
+    double lo_power = find_system_power(lo, power, capacity);
+    double mid_power = find_system_power(mid, power, capacity);
+    double hi_power = find_system_power(hi, power, capacity);
 
 
-    if((old_result != mid_power) && (fabs(old_result - mid_power) < 0.001)){
+    if ((old_result != mid_power) && (fabs(old_result - mid_power) < 0.001)) {
         return mid_power;
     }
 
     old_result = mid_power;
 
-    if(hi_power >= lo_power){
-        return find_best(mid,hi,old_result,power,capacity);
+    if (hi_power >= lo_power) {
+        return find_best(mid, hi, old_result, power, capacity);
     }
-    else{
-        return find_best(lo,mid,old_result,power,capacity);
+    else {
+        return find_best(lo, mid, old_result, power, capacity);
     }
 }
 
-double get_result(vector<int> power, vector<int> capacity) { 
+
+double get_result(vector<int> power, vector<int> capacity) {
     int upper_bound = max_vector(capacity);
-    return find_best(0,upper_bound, MAXFLOAT, power, capacity);
+    return find_best(0, upper_bound, MAXFLOAT, power, capacity);
 }
-
-void print_output(double result) {
-    ofstream out("servere.out");
-    out << fixed << setprecision(1) << result << "\n";
-}
-    
 
 int main() {
     std::ios::sync_with_stdio(false);
 
     int nr_servers;
-
-    ifstream in("servere.in");
-    in >> nr_servers;
-
     vector<int> power;
     vector<int> capacity;
+
+    // Read input
+    ifstream in("servere.in");
+    in >> nr_servers;
 
     for (int i = 0; i < nr_servers; i++) {
         int e;
@@ -78,8 +73,12 @@ int main() {
         in >> e;
         capacity.push_back(e);
     }
+    in.close();
 
-    print_output(get_result(power,capacity));
-     
+    // Write output
+    ofstream out("servere.out");
+    out << fixed << setprecision(1) << get_result(power, capacity) << "\n";
+    out.close();
+
     return 0;
 }
