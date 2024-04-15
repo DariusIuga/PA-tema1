8c00e0a3d984b4620a037d6985da108d3a21aaf6
DariusIuga <dariusiuga2@gmail.com>
2024-04-15 23:01:36 +0300

    Ex 2 colorare done

--- check_public/colorare.cpp
+++ check_public/colorare.cpp
@@ -3,61 +3,24 @@ using namespace std;
 
 const unsigned int MOD = 1'000'000'007;
 
-
-// unsigned int get_result(const vector<char>& zones) {
-//     // I'll use a greedy algorithm that calculates the number of possible combinations for k zones based on the number of combinations for k-1 zones, the type of the previous tile and the type of the current tile.
-
-//     /* Base cases:
-
-//     1) zones[0] == 'V' => nr_current = 3
-//     2) zones[0] == 'H' => nr_current = 6
-
-//     */
-//     unsigned int nr_paintings;
-//     if (zones[0] == 'V') {
-//         nr_paintings = 3;
-//     }
-//     else {
-//         nr_paintings = 6;
-//     }
-
-//     /* There are 4 general cases:
-
-//     1) prev_zone == 'V', current_zone == 'V' => nr_current = 2 * nr_prev
-//     2) prev_zone == 'V', current_zone == 'H' => nr_current = 2 * nr_prev
-//     1) prev_zone == 'H', current_zone == 'V' => nr_current = 1 * nr_prev
-//     1) prev_zone == 'H', current_zone == 'H' => nr_current = 3 * nr_prev
-
-//     */
-//     for (int i = 1;i < zones.size();i++) {
-//         if (zones[i - 1] == 'V') {
-//             nr_paintings = (nr_paintings * 2) % MOD;
-//         }
-//         else if (zones[i] == 'H') {
-//             nr_paintings = (nr_paintings * 3) % MOD;
-//         }
-//     }
-
-//     return nr_paintings;
-// }
-
 struct Group {
     int nr_zones;
     char type;
 };
 
-int fast_powers(int a, int b) {
-    if (b == 0) {
+unsigned long long log_power(unsigned int base, unsigned int exp) {
+    // Computes base ^ exp in O(log(n)) time complexity.
+    if (exp == 0) {
         return 1;
     }
-    long long result = 1;
-    while (b > 0) {
-        if (b % 2 == 1) {
-            result = (result * a) % MOD;
+    unsigned long long result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % MOD;
         }
 
-        a = ((long long)a * a) % MOD;
-        b /= 2;
+        base = ((long long)base * base) % MOD;
+        exp /= 2;
     }
 
     return result;
@@ -75,10 +38,10 @@ unsigned int get_result(const vector<Group>& groups) {
     */
     unsigned int nr_paintings;
     if (groups[0].type == 'V') {
-        nr_paintings = (3 * fast_powers(2, groups[0].nr_zones - 1)) % MOD;
+        nr_paintings = (3 * log_power(2, groups[0].nr_zones - 1)) % MOD;
     }
     else {
-        nr_paintings = (6 * fast_powers(3, groups[0].nr_zones - 1)) % MOD;
+        nr_paintings = (6 * log_power(3, groups[0].nr_zones - 1)) % MOD;
     }
 
     /* There are 4 general cases:
@@ -94,19 +57,19 @@ unsigned int get_result(const vector<Group>& groups) {
         if (groups[i - 1].type == 'V') {
             nr_paintings = (nr_paintings * 2) % MOD;
             if (groups[i].type == 'V') {
-                nr_paintings = (nr_paintings * fast_powers(2, groups[i].nr_zones - 1)) % MOD;
+                nr_paintings = (nr_paintings * log_power(2, groups[i].nr_zones - 1)) % MOD;
             }
             else {
-                nr_paintings = (nr_paintings * fast_powers(3, groups[i].nr_zones - 1)) % MOD;
+                nr_paintings = (nr_paintings * log_power(3, groups[i].nr_zones - 1)) % MOD;
             }
         }
         else {
             if (groups[i].type == 'V') {
-                nr_paintings = (nr_paintings * fast_powers(2, groups[i].nr_zones - 1)) % MOD;
+                nr_paintings = (nr_paintings * log_power(2, groups[i].nr_zones - 1)) % MOD;
             }
             else {
                 nr_paintings = (nr_paintings * 3) % MOD;
-                nr_paintings = (nr_paintings * fast_powers(3, groups[i].nr_zones - 1)) % MOD;
+                nr_paintings = (nr_paintings * log_power(3, groups[i].nr_zones - 1)) % MOD;
             }
         }
 
@@ -128,8 +91,7 @@ int main() {
 
     for (int i = 0; i < nr_groups; i++) {
         Group group;
-        // int nr_zones_in_group;
-        // char type;
+
         in >> group.nr_zones >> group.type;
         groups.push_back(group);
     }
@@ -137,9 +99,6 @@ int main() {
 
     // Write output
     ofstream out("colorare.out");
-    // for (const Group& group : groups) {
-    //     out << group.nr_zones << " " << group.type << " ";
-    // }
     out << get_result(groups) << "\n";
 
 
